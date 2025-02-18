4d37da52a143e5174bfc09d04ddd7492fbe26fc7
Padmavathi014 <padma.rm14@gmail.com>
2025-02-18 21:10:56 +0530

    thrid commit

---
+++ Area_of_Circle.java
@@ -0,0 +1,14 @@
+package module1programs;
+
+public class Area_of_Circle 
+{
+	public static void main(String[] args)
+	{
+		double pi=3.14;
+		int r=10;
+		double area=pi*r*r;
+		System.out.println(area);
+		
+	}
+
+}
---
+++ CollectionProgram.java
@@ -0,0 +1,24 @@
+package module1programs;
+import java.util.ArrayList;
+import java.util.Collection;
+public class CollectionProgram
+{
+	public static void main(String[] args)
+	{
+	Collection c1= new ArrayList();
+	c1.add("Ram");
+	c1.add("42");
+	c1.add("M");
+	c1.add("78000");
+	c1.add("false");
+	c1.add("Ram");
+	c1.add("null");
+	System.out.println(c1);
+}
+}
+	
+	
+	
+	
+
+
